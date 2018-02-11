c25dea5b066f52bbd87d901e14839e9f4a6ce1b7
owen <ofscorey@gmail.com>
2018-02-11 18:51:34 +0000

    Renamed json

--- thermodynamics_convergence.py
+++ thermodynamics_convergence.py
@@ -55,6 +55,6 @@ except:
 
 for e in range(0, 10, parameters['epochs'] + 10):
 
-	saved_state = args.input_path + "/trained_rbm.pytorch." + e
+	saved_state = args.input_path + "/trained_rbm.pytorch." + str(e)
 	filename = temperature + "_thermo_convergence.data"
 	sample_and_save(temperature, saved_state, parameters, filename, e)
