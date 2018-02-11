48cc1a475aff66390ff0819592e4dc25bab43829
owen <ofscorey@gmail.com>
2018-02-11 19:40:27 +0000

    Update cuda support

--- ising_methods_new.py
+++ ising_methods_new.py
@@ -169,7 +169,7 @@ def hidden_from_visible(visible, W, h_bias):
 		new_states: Tensor containing binary (1 or 0) states of the hidden variables
 		probability: Tensor containing probabilities P(H_i = 1| {V})
 	"""
-	dtype = visible.type()
+	dtype = torch.cuda.FloatTensor
 	probability = torch.sigmoid(F.linear(visible, W, h_bias))
 	random_field = torch.rand(probability.size()).type(dtype)
 	new_states = sample_probability(probability, random_field)
@@ -188,7 +188,7 @@ def visible_from_hidden(hid, W, v_bias):
 		new_states: Tensor containing binary (1 or 0) states of the visible variables
 		probability: Tensor containing probabilities P(V_i = 1| {H})
 	"""
-	dtype = hid.type()
+	dtype = torch.cuda.FloatTensor
 	probability = torch.sigmoid(F.linear(hid, W.t(), v_bias))
 	random_field = torch.rand(probability.size())#.type(dtype)
 	new_states = sample_probability(probability, random_field)
