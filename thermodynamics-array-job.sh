bdd8df7724d630d9e5c0c87540864c967621f2a4
owen <ofscorey@gmail.com>
2018-02-11 17:37:04 +0000

    wrote job script

--- thermodynamics-array-job.sh
+++ thermodynamics-array-job.sh
@@ -9,4 +9,4 @@
 
 module load igmm/apps/anaconda/5.0.0.1
 source activate owen-pytorch
-python3 thermodynamics-convergence.py --json thermodynamics.json --training_data ./training_data/state$SGE_TASK_ID.txt --input_path ./data/$SGE_TASK_ID  --output_path ./data/$SGE_TASK_ID
+python3 thermodynamics-convergence.py --json thermodynamics.json --training_data ../rbm-pytorch-refactor/training_data/state$SGE_TASK_ID.txt --input_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID  --output_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID
