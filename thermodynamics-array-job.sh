06bab51219c8a8f4543407381b3c4e6ecd725778
owen <ofscorey@gmail.com>
2018-02-11 18:36:36 +0000

    Fixed parser

--- thermodynamics-array-job.sh
+++ thermodynamics-array-job.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 #$ -cwd
-#$ -N training
-#$ -l h_rt 04:00:00
+#$ -N thermodynamics
+#$ -l h_rt=04:00:00
 #$ -pe gpu 1
-#$ -l h_vmem 16G
+#$ -l h_vmem=16G
 
 . /etc/profile.d/modules.sh
 
 module load igmm/apps/anaconda/5.0.0.1
 source activate owen-pytorch
-python3 thermodynamics-convergence.py --json thermodynamics.json --training_data ../rbm-pytorch-refactor/training_data/state$SGE_TASK_ID.txt --input_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID  --output_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID
+python3 thermodynamics_convergence.py --cuda True --json thermodynamics.json --training_data ../rbm-pytorch-refactor/training_data/state$SGE_TASK_ID.txt --input_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID  --output_path ../rbm-pytorch-refactor/data/$SGE_TASK_ID
