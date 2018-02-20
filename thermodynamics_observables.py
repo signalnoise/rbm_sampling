425a36ac13113fe2c3359e7cac5c0d4b36d74d26
owen <ofscorey@gmail.com>
2018-02-20 17:12:05 +0000

    Added bootstrapping

--- thermodynamics_observables.py
+++ thermodynamics_observables.py
@@ -65,6 +65,9 @@ for i in range(1, 31, 1):
 
 	training_data = "/exports/csce/eddie/ph/groups/rbm_ml/owen-data/training_data/state" + str(i) + ".txt"
 
+	with open(training_data) as file:
+		temperature = float(file.readline())
+
 	train_loader = DataLoader(rbm_interface.CSV_Ising_dataset(training_data, size=8), 
 							shuffle=True, batch_size=100000, drop_last=True)
 	for i, (data, target) in enumerate(train_loader):
@@ -73,9 +76,6 @@ for i in range(1, 31, 1):
 	with open(args.input_path + "training_data_observables.txt", "a") as file:
 		file.write("{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\t{:f}\n".format(temperature, 
 					mag[0], mag[1], susc[0], susc[1], energy[0], energy[1], heatc[0], heatc[1]))
-
-	with open(training_data) as file:
-		temperature = float(file.readline())
 
 	saved_state = input_dir + "/trained_rbm.pytorch.1000"
 	filename = args.input_path + "temp_graph_machines.txt"
