e959786ddbdec3c2f84b43cdaebee0559b73904c
owen <ofscorey@gmail.com>
2018-03-11 19:28:38 +0000

    updates

---
+++ histograms.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#$ -cwd
+#$ -N thermodynamics
+#$ -l h_rt=04:00:00
+#$ -pe gpu 1
+#$ -l h_vmem=16G
+
+. /etc/profile.d/modules.sh
+
+module load igmm/apps/anaconda/5.0.0.1
+source activate owen-pytorch
+python3 plotter.py --json thermodynamics-params.json
