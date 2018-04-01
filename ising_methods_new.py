bf779c447abe4be0a9dd8fbc40358358f463d48a
owen <ofscorey@gmail.com>
2018-04-01 23:34:45 +0100

    help

--- ising_methods_new.py
+++ ising_methods_new.py
@@ -66,8 +66,8 @@ def magnetisation(spin_state):
 	Returns:
 		Torch tensor containing the magnetisations of the states
 	"""
-	
-	return torch.mean(spin_state, dim=2).abs_()*spin_state.shape[2]
+	print(spin_state.shape[2])
+	return torch.mean(spin_state, dim=2).abs_()*spin_state.numpy().shape[2]
 
 def ising_observables(states, L, temperature):
 	"""Computes observables given a full dataset for a certain temperature.
